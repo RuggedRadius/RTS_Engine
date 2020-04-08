e06a7b431a679d295f490aa6c8dff2231b3a8ce1
Ben Royans <ben.royans@gmail.com>
2020-04-08 22:41:56 +0800

    Some animation tested

--- Assets/My Assets/Scripts/Unit/Unit.cs
+++ Assets/My Assets/Scripts/Unit/Unit.cs
@@ -24,6 +24,17 @@ public class Unit : MonoBehaviour
     [SerializeField]
     public GameObject prefab;
 
+    [Header("State")]
+    public bool moving;
+
+    [Header("References")]
+    [SerializeField]
+    private Animator anim;
+
+    [Header("Settings")]
+    [SerializeField]
+    private float destinationTolerance;
+    
     private NavMeshAgent agent;
 
     void Start()
@@ -31,8 +42,49 @@ public class Unit : MonoBehaviour
         agent = this.gameObject.AddComponent<NavMeshAgent>();
     }
 
+    private void Update()
+    {
+        // Update animator values
+        updateAnimatorValues();
+
+        // Check if close to destination
+        checkDestinationTolerance();
+
+
+    }
+
     public void move(Vector3 destination)
-    {      
-        agent.SetDestination(destination);        
+    {
+        moving = true;
+        agent.SetDestination(destination);
+    }
+
+    private void checkDestinationTolerance()
+    {
+        // If all 3 components of the vector 3 are withinn tolerance
+        if (
+            distanceToDestination(agent.destination).x < destinationTolerance &&
+            distanceToDestination(agent.destination).y < destinationTolerance &&
+            distanceToDestination(agent.destination).z < destinationTolerance
+            )
+        {
+            // No more movings
+            agent.destination = agent.transform.position;
+            moving = false;
+        }
+    }
+
+    private void updateAnimatorValues()
+    {
+        anim.SetBool("Moving", moving);
+    }
+
+    private Vector3 distanceToDestination(Vector3 _destination)
+    {
+        return new Vector3( 
+            _destination.x - agent.transform.position.x,
+            _destination.y - agent.transform.position.y,
+            _destination.z - agent.transform.position.z
+            );
     }
 }
